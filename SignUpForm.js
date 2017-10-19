a137ef387a60f297d2f5d9a77be5aa85fc7324da
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-10-19 15:09:00 -0400

    created object to store new users

--- SignUpForm.js
+++ SignUpForm.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
-    $('select').material_select();
- });
+
   
+//Create an Object that will store a users info
+
+let newUser = {};
+
+function storeNewUser(){
+    //
+
+    //Name
+    let firstName = document.getElementById("#first_name");
+    let lastName = document.getElementById("#last_name ");
+
+    newUser.name = firstName + " " + lastName;
+
+    //Email
+    newUser.email = document.getElementById("#email");
+
+    //Phone Number
+    newUser.phoneNumber = document.getElementById("#phone_number");
+
+}
